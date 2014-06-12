f89a29f228c0271d966ac27976eb7e60f2f07e84
Jonathan Scott <jonathan@jscott.me>
2014-06-12 20:29:34 +0100

    Change interface

--- player.js
+++ player.js
@@ -7,6 +7,10 @@ Interface.prototype.code_chosen = function(n) {
 Interface.prototype.guess_made = function(guess, state) {
     console.log("New guess made:", guess);
     console.log("New state:", state);
+};
+
+Interface.prototype.game_finished = function(state) {
+    console.log("Game finished:", state);
 }
 
 
@@ -16,19 +20,59 @@ function Game(interface_class, player1_class, player2_class) {
     this.player2 = new player2_class();
 
     this.state = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+    this.turn = 0;
 }
 
 Game.prototype.start = function() {
     this.player1.provide_code(function(code) {
         this.interface.code_chosen(code);
+        this.code = code;
 
-        while (!this.game_finished()) {
-            guess = this.player2.take_turn(this.state)
-        }
+        this.next_turn();
     });
 }
 
+Game.prototype.next_turn = function() {
+    if (this.game_finished()) {
+        this.interface.game_finished(this.state);
+    } else {
+        this.player2.take_turn(this.state, function(guess) {
+            result = {
+                "misses": 0,
+                "partially_correct": 0,
+                "correct": 0
+            };
+
+            for (var i in guess) {
+                if (guess[i] == code[i]) {
+                    result["correct"] += 1;
+                } else if (guess[i] in code) {
+                    result["partially_correct"] += 1;
+                } else {
+                    result["misses"] += 1;
+                }
+            }
+
+            this.state[this.turn] = result;
+            this.turn += 1;
+
+            this.interface.guess_made(guess, this.state);
+            setTimeout(this.next_turn, 2000);
+        });
+    }
+}
+
 Game.prototype.game_finished = function() {
+    if (this.turn == 10) {
+        return true;
+    }
+
+    for (var i in this.state) {
+        if (this.state[i].correct == 4) {
+            return true;
+        }
+    }
+
     return false;
 }
 
@@ -37,6 +81,11 @@ function Player() {}
 
 Player.prototype.take_turn = function(state, take_turn_callback) {
     // This indicates that the player can take their first turn
+    var code = [];
+    for (i = 0; i < 4; i++) {
+        code.push(Math.floor(Math.random() * 5));
+    }
+    take_turn_callback(code);
 }
 
 Player.prototype.provide_code = function(provide_code_callback) {
@@ -46,3 +95,5 @@ Player.prototype.provide_code = function(provide_code_callback) {
     }
     provide_code_callback(code);
 }
+
+g = Game(Interface, UserPlayer, AiPlayer)
