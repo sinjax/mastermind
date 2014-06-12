0fff3bad38b923453b8835d9a52f57da4ba73310
Sina Samangooei <sinjax@gmail.com>
2014-06-12 19:39:07 +0100

    initial commit

--- player.js
+++ player.js
@@ -1,11 +1,16 @@
-function player(){
-	this.provide_guess = function() {
-		console.log("Cheese");
-	};
-	this.recieve_feedback = function(feedback){
 
-	};
+function player(){
 }
 
-x = player();
+player.prototype.provide_guess = function() {
+	console.log("Cheese");
+};
+player.prototype.recieve_feedback = function(feedback){
+
+};
+player.prototype.provide_code = function() {
+}
+
+x = new player();
 x.provide_guess();
+console.log(x.provide_code())
