4393e02b7b32be1849aa1c54b63013ab703e28ed
huw <huw@kali.(none)>
2014-06-12 20:13:30 +0100

    array of combos

---
+++ combos.js
@@ -0,0 +1,18 @@
+//Generate every set of codes
+var all_combos = []
+for(var i = 0; i < 6; i++){
+	for(j=0; j < 6; j++){
+		for(k=0; k< 6; k++){
+			for n=0; n < 6; n++){
+				combo = [i,j,k,n];
+				all_combos.push(combo);
+			}
+		}
+	}
+
+}
+//Test how many times it takes the player to guess them
+
+
+
+//Average it out	
