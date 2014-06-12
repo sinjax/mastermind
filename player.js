1527ca81b1e5ccf273a18636a253463e8fc098fc
Jonathan Scott <jonathan@jscott.me>
2014-06-12 20:14:12 +0100

    Change interface

--- player.js
+++ player.js
@@ -15,17 +15,17 @@ function Game(interface_class, player1_class, player2_class) {
     this.player1 = new player1_class();
     this.player2 = new player2_class();
 
-    this.state = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    this.state = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 }
 
 Game.prototype.start = function() {
-    var code = this.player1.provide_code();
+    this.player1.provide_code(function() {
+        this.interface.code_chosen(code);
 
-    this.interface.code_chosen(code);
-
-    while (!this.game_finished()) {
-        guess = this.player2.take_turn(this.state)
-    }
+        while (!this.game_finished()) {
+            guess = this.player2.take_turn(this.state)
+        }
+    });
 }
 
 Game.prototype.game_finished = function() {
@@ -39,10 +39,10 @@ Player.prototype.take_turn = function(state) {
     // This indicates that the player can take their first turn
 }
 
-Player.prototype.provide_code = function() {
+Player.prototype.provide_code = function(provide_code_callback) {
     var code = [];
     for (i = 0; i < 4; i++) {
         code.push(Math.floor(Math.random() * 5));
     }
-    return code;
+    provide_code_callback(code);
 }
